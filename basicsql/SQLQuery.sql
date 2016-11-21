825e3f9a20d83e808ec545d8ef65f38c3c76f8ba
Eidan Cohen <eidanch@gmail.com>
2016-11-21 19:48:38 +0200

    Some basic sql queries

---
+++ basicsql/SQLQuery.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE People (
+ID int IDENTITY(1,1) PRIMARY KEY,
+Name varchar(255)
+);
+
+CREATE TABLE Items (
+ID int IDENTITY(1,1) PRIMARY KEY,
+PersonID int FOREIGN KEY REFERENCES People(ID),
+Name varchar(255)
+);
+
+INSERT INTO People VALUES ('Eidan');
+INSERT INTO People VALUES ('Doron');
+INSERT INTO Items VALUES (1,'Laptop');
+
+SELECT * FROM People;
