245bae0d436124bbdfdd9b9eb6564ad16dd5d62a
eidanch <USER1@DOMAIN.local>
2016-11-22 09:04:06 +0200

    Added basic C# SQL example

---
+++ basicsql/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        const int NAME_INDEX = 0;
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=\"c:\\users\\user1\\documents\\visual studio 2010\\Projects\\ConsoleApplication1\\ConsoleApplication1\\Database1.mdf\";Integrated Security=True;User Instance=True");
+            connection.Open();
+
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT Name, ID FROM Students WHERE ID = 2;";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(NAME_INDEX));
+            }
+
+            connection.Close();
+        }
+    }
+}
