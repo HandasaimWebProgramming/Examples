f1d75879a6d9df13cb6266aee26044c7eed84eb5
eidanch <USER1@DOMAIN.local>
2017-02-20 15:16:46 +0200

    Added first asp example

---
+++ basic-asp-forms/WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <u><%= s %></u>
+    <form id="form1" runat="server" action="WebForm2.aspx">
+    <div>
+        <input type="text" placeholder="Enter some text" name="something" />
+        <br />
+        <input type="submit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ basic-asp-forms/WebApplication1/WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        string connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"|DataDirectory|\\Database1.mdf\";Integrated Security=True;User Instance=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = String.Format("INSERT INTO Arrg VALUES ('{0}');", Request.Form["something"]);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
