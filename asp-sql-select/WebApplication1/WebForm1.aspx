c771fe6d87ef19425607d947be5d93796906fcad
eidanch <USER1@DOMAIN.local>
2017-02-27 15:19:42 +0200

    Added example from class

---
+++ asp-sql-select/WebApplication1/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    Id
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Email
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <%= tableContent %>
+        </tbody>
+    </table>
+    <form id="form1" runat="server">
+    <div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ asp-sql-select/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string tableContent = "";
+        public string template = @"<tr>
+                <td>{0}</td>
+                <td>{1}</td>
+                <td>{2}</td>
+            </tr>
+";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connetionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"|DataDirectory|\\Database1.mdf\";Integrated Security=True;User Instance=True";
+            SqlConnection connection = new SqlConnection(connetionString);
+            connection.Open();
+
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM People;";
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string row = String.Format(template, reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                tableContent += row;
+            }
+            reader.Close();
+
+            connection.Close();
+        }
+    }
+}
