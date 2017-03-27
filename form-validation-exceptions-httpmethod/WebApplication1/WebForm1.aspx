70b707eb1c01b0211202759eccc032ca5dba9f1e
eidanch <USER1@DOMAIN.local>
2017-03-27 16:41:15 +0300

    Added last lesson
    about server exceptions, http method (POST/GET) and javascript form
    validation

---
+++ form-validation-exceptions-httpmethod/WebApplication1/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        function validate() {
+            var name = document.getElementById("name").value;
+            if (name.length < 8) {
+                alert("Error");
+                return false;
+            }
+            return true;
+        }
+    </script>
+</head>
+<body>
+<p><%= message %></p>
+    <form id="form1" runat="server" action="WebForm1.aspx" onsubmit="return validate();">
+    <div>
+    <input type="text" name="name" id="name" />
+    </div>
+    <div>
+    <input type="submit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ form-validation-exceptions-httpmethod/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"|DataDirectory|\\Database1.mdf\";Integrated Security=True;User Instance=True";
+
+        public string message = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                string name = Request.Form["name"];
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = String.Format("INSERT INTO Names VALUES ('{0}');", name);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    message = "Success!";
+                }
+                catch (SqlException)
+                {
+                    message = "User already exists!";
+                }
+                connection.Close();
+            }
+        }
+    }
+}
